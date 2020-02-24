8b8a669c38356173f17b0c818d774e25c2ce5e61
tommytroen <tommytroen@gmail.com>
2020-02-24 09:54:53 +0100

    ci: increase delay between retries in nexus-staging plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,7 @@ nexusStaging {
     username = System.getenv("SONATYPE_USERNAME")
     password = System.getenv("SONATYPE_PASSWORD")
     packageGroup = "no.nav"
+    delayBetweenRetriesInMillis = 5000
 }
 
 nexusPublishing {
