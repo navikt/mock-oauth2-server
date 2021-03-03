8def7b967ad7aa327d108cda49c1c55568c513b8
Tommy Trøen <tommy.troen@nav.no>
2021-03-03 22:55:22 +0100

    feat: configure StandaloneMockOAuth2Server.kt from env and json * support loading OAuth2Config from env var or json file

--- src/main/kotlin/no/nav/security/mock/oauth2/StandaloneMockOAuth2Server.kt
+++ src/main/kotlin/no/nav/security/mock/oauth2/StandaloneMockOAuth2Server.kt
@@ -1,31 +1,56 @@
 package no.nav.security.mock.oauth2
 
-import java.net.InetAddress
-import java.net.InetSocketAddress
+import no.nav.security.mock.oauth2.StandaloneConfig.hostname
+import no.nav.security.mock.oauth2.StandaloneConfig.oauth2Config
+import no.nav.security.mock.oauth2.StandaloneConfig.port
 import no.nav.security.mock.oauth2.http.NettyWrapper
 import no.nav.security.mock.oauth2.http.OAuth2HttpResponse
 import no.nav.security.mock.oauth2.http.route
+import java.io.File
+import java.io.FileNotFoundException
+import java.net.InetAddress
+import java.net.InetSocketAddress
 
-data class Configuration(
-    val server: Server = Server()
-) {
-    data class Server(
-        val hostname: InetAddress = "SERVER_HOSTNAME".fromEnv()?.let { InetAddress.getByName(it) } ?: InetSocketAddress(0).address,
-        val port: Int = "SERVER_PORT".fromEnv()?.toInt() ?: 8080
-    )
+object StandaloneConfig {
+    const val JSON_CONFIG = "JSON_CONFIG"
+    const val JSON_CONFIG_PATH = "JSON_CONFIG_PATH"
+    const val SERVER_HOSTNAME = "SERVER_HOSTNAME"
+    const val SERVER_PORT = "SERVER_PORT"
+
+    fun hostname(): InetAddress = SERVER_HOSTNAME.fromEnv()
+        ?.let { InetAddress.getByName(it) } ?: InetSocketAddress(0).address
+
+    fun port(): Int = SERVER_PORT.fromEnv()?.toInt() ?: 8080
+
+    fun oauth2Config(): OAuth2Config = with(JSON_CONFIG.fromEnv() ?: JSON_CONFIG_PATH.fromEnv("config.json").readFile()) {
+        if (this != null) {
+            OAuth2Config.fromJson(this)
+        } else {
+            OAuth2Config(
+                interactiveLogin = true,
+                httpServer = NettyWrapper()
+            )
+        }
+    }
+
+    private fun String.readFile(): String? =
+        try {
+            File(this).readText()
+        } catch (e: FileNotFoundException) {
+            null
+        }
 }
 
 fun main() {
-    val config = Configuration()
     MockOAuth2Server(
-        OAuth2Config(
-            interactiveLogin = true,
-            httpServer = NettyWrapper()
-        ),
+        oauth2Config(),
         route("/isalive") {
             OAuth2HttpResponse(status = 200, body = "alive and well")
         }
-    ).start(config.server.hostname, config.server.port)
+    ).apply {
+        start(hostname(), port())
+    }
 }
 
+fun String.fromEnv(default: String): String = System.getenv(this) ?: default
 fun String.fromEnv(): String? = System.getenv(this)
