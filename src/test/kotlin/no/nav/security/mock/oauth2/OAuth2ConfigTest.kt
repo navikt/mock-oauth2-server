fc0fc8ceec816441103e6b9293773a17bd460d73
ybelMekk <youssef.bel.mekki@nav.no>
2021-11-24 00:29:32 +0100

    add: Oauth2Config tests for generating singing keys tru json. Update Ouath2Config to handle OAuth2TokenProvider with nested objects.

--- src/test/kotlin/no/nav/security/mock/oauth2/OAuth2ConfigTest.kt
+++ src/test/kotlin/no/nav/security/mock/oauth2/OAuth2ConfigTest.kt
@@ -1,12 +1,14 @@
 package no.nav.security.mock.oauth2
 
 import com.fasterxml.jackson.databind.JsonMappingException
+import com.nimbusds.jose.jwk.KeyType
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.matchers.Matcher
 import io.kotest.matchers.MatcherResult
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.types.beInstanceOf
 import no.nav.security.mock.oauth2.FullConfig.configJson
 import no.nav.security.mock.oauth2.HttpServerConfig.mockWebServerWithGeneratedKeystore
@@ -16,6 +18,9 @@ import no.nav.security.mock.oauth2.HttpServerConfig.nettyWithProvidedKeystore
 import no.nav.security.mock.oauth2.HttpServerConfig.withMockWebServerWrapper
 import no.nav.security.mock.oauth2.HttpServerConfig.withNettyHttpServer
 import no.nav.security.mock.oauth2.HttpServerConfig.withUnknownHttpServer
+import no.nav.security.mock.oauth2.SigningKey.signingJsonGenerated
+import no.nav.security.mock.oauth2.SigningKey.signingJsonSpecified
+import no.nav.security.mock.oauth2.SigningKey.signingJsonUnsupported
 import no.nav.security.mock.oauth2.http.MockWebServerWrapper
 import no.nav.security.mock.oauth2.http.NettyWrapper
 import org.intellij.lang.annotations.Language
@@ -48,6 +53,26 @@ internal class OAuth2ConfigTest {
         config.tokenCallbacks.map {
             it.issuerId()
         }.toList() shouldContainAll listOf("issuer1", "issuer2")
+    }
+
+    @Test
+    fun `create tokenProvider with generated signing algorithm EC`() {
+        val config = OAuth2Config.fromJson(signingJsonGenerated)
+        config.tokenProvider.publicJwkSet().keys[0].keyType.value shouldBe KeyType.EC.value
+    }
+
+    @Test
+    fun `create tokenProvider with specified signing algorithm EC`() {
+        val config = OAuth2Config.fromJson(signingJsonSpecified)
+        config.tokenProvider.publicJwkSet().keys[0].keyType.value shouldBe KeyType.EC.value
+        config.tokenProvider.publicJwkSet("issuer0").keys[0].keyID shouldBe "issuer0"
+    }
+
+    @Test
+    fun `create config from json with an unsupported algorithm should throw Oauth2Exception`() {
+        shouldThrow<JsonMappingException> {
+            OAuth2Config.fromJson(signingJsonUnsupported)
+        }.message shouldContain "Unsupported algorithm: EdDSA"
     }
 
     @Test
@@ -142,6 +167,42 @@ object FullConfig {
         }
       ]
     }
+    """.trimIndent()
+}
+
+object SigningKey {
+    @Language("json")
+    val signingJsonGenerated = """
+        {
+        "tokenProvider" : {
+            "keyProvider" : {
+               "algorithm" : "ES256"
+            }
+          }
+        }
+    """.trimIndent()
+
+    @Language("json")
+    val signingJsonSpecified = """
+        {
+        "tokenProvider" : {
+            "keyProvider" : {
+               "initialKeys" : "{\"kty\": \"EC\",\"d\": \"o9INzHyU_I97djF36YQRpHCJxFTgDTbS1OtwUnHc34U\",\"use\":\"sig\",\"crv\": \"P-256\",\"kid\": \"issuer0\",\"x\": \"umybCYzE-VX_UAIJaX3wc-GTOgB7WDp7A3JJAKW_hqU\",\"y\": \"m_sCzuMjiBSQ7At9yNktMQvE1cCKq68jO7wnRczwKw8\"}",
+               "algorithm" : "ES256"
+            }
+          }
+        }
+    """.trimIndent()
+
+    @Language("json")
+    val signingJsonUnsupported = """
+        {
+        "tokenProvider" : {
+            "keyProvider" : {
+               "algorithm" : "EdDSA"
+            }
+          }
+        }
     """.trimIndent()
 }
 
